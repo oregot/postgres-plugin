260971627861ba232866c196f6c8fd22fcd75e9c
Oleg Ilin <on.ilyin@gmail.com>
2016-03-25 14:18:46 +0300

    added VIP; added node list

--- deployment_scripts/deploy.pp
+++ deployment_scripts/deploy.pp
@@ -1,6 +1,13 @@
 notice('MODULAR: postgres_database.pp')
 
+$network_metadata = hiera_hash('network_metadata')
+$nodes_list       = keys($network_metadata[nodes])
+$pgsql_vip   = $network_metadata['vips']['pgsql']['ipaddr']
+$postgres_resource_name = 'p_pgsql'
+$postgres_vip_name = 'vip__pgsql'
 
+
+# Installing and configure postgresql
 class { 'postgresql::globals':
   encoding => 'UTF8',
 } ->
@@ -11,102 +18,80 @@ class { 'postgresql::server':
 
 } ->
 
+# Stopping postgresql befor adding it to the pacemaker
 exec { "service postgresql stop":
   path    => ["/usr/bin", "/usr/sbin"]
 }
 
-
-notice('MODULAR: postgres_database.pp')
-
-
-$postgres_resource_name = 'p_pgsql'
-$postgres_vip_name = 'vip__pgsql'
-
-$postgres_vip_ip = '1.1.1.1'
-$postgres_vip_mask = '24'
-
-  cs_resource {$postgres_resource_name:
-      ensure          => present,
-      primitive_class => 'ocf',
-      provided_by     => 'hearbeat',
-      primitive_type  => 'pgsql',
-#      metadata        => {
-#        'migration-threshold' => 'INFINITY',
-#        'failure-timeout'     => '180s'
-#      },
-      parameters => {
-        'pgctl'     => '/usr/lib/postgresql/9.3/bin/pg_ctl',
-        'psql'      => '/usr/lib/postgresql/9.3/bin/psql',
-        'pgdata'    => '/var/lib/postgresql/9.3/main',
-        'rep_mode'  => 'sync',
-        'node_list' => 'node-20',
-        'master_ip' => '1.1.1.1',
-        'restart_on_promote' => 'true',
-      },
-      complex_type => 'master',
-      ms_metadata  => {
-        'notify'          => 'true',
-        'clone-node-max'  => '1',
-        'master-max'      => '1',
-        'master-node-max' => '3',
-        'target-role'     => 'Master'
-      },
-      operations   => {
-        'monitor'  => {
-        'interval' => '4',
-        'timeout'  => '60',
-        'on-fail'  => 'restart',
-      },
-      'monitor:Master' => {
-        'role'         => 'Master',
-        'interval'     => '3',
-        'timeout'      => '60',
-        'on-fail'  => 'restart',
-        },
-      'start'  => {
-        'interval' => '0',
-        'timeout'  => '60',
-        'on-fail'  => 'restart',
-      },
-      'stop'  => {
-        'interval' => '0',
-        'timeout'  => '60',
-        'on-fail'  => 'block',
-      },
-      'promote'  => {
-        'interval' => '0',
-        'timeout'  => '60',
-        'on-fail'  => 'restart',
-      },
-      'demote'  => {
-        'interval' => '0',
-        'timeout'  => '60',
-        'on-fail'  => 'stop',
-      },
-      'notify'  => {
-        'interval' => '0',
-        'timeout'  => '60',
-      },
-
-      },
-    }
-
-cs_primitive { "${postgres_vip_name}":
+# Creating postgresql resource in pacemaker
+cs_resource {$postgres_resource_name:
+  ensure          => present,
   primitive_class => 'ocf',
-  primitive_type  => 'IPaddr2',
-  provided_by     => 'heartbeat',
-  parameters      => { 'ip' => "${postgres_vip_ip}", 'cidr_netmask' => "${postgres_vip_mask}" },
-  operations      => {'monitor' => { 'interval' => '10' , 'timeout' => '60' },
-                      'start' => { 'interval' => '0' , 'timeout' => '60' },
-                      'stop' => { 'interval' => '0' , 'timeout' => '60' },
-}
+  provided_by     => 'hearbeat',
+  primitive_type  => 'pgsql',
+  parameters => {
+    'pgctl'     => '/usr/lib/postgresql/9.3/bin/pg_ctl',
+    'psql'      => '/usr/lib/postgresql/9.3/bin/psql',
+    'pgdata'    => '/var/lib/postgresql/9.3/main',
+    'rep_mode'  => 'sync',
+    'node_list' => "$nodes_list",
+    'master_ip' => "$pgsql_vip",
+    'restart_on_promote' => 'true',
+  },
+  complex_type => 'master',
+  ms_metadata  => {
+    'notify'          => 'true',
+    'clone-node-max'  => '1',
+    'master-max'      => '1',
+    'master-node-max' => '3',
+    'target-role'     => 'Master'
+  },
+  operations   => {
+    'monitor'  => {
+      'interval' => '4',
+      'timeout'  => '60',
+      'on-fail'  => 'restart',
+    },
+    'monitor:Master' => {
+      'role'         => 'Master',
+      'interval'     => '3',
+      'timeout'      => '60',
+      'on-fail'  => 'restart',
+     },
+    'start'  => {
+      'interval' => '0',
+      'timeout'  => '60',
+      'on-fail'  => 'restart',
+    },
+    'stop'  => {
+      'interval' => '0',
+      'timeout'  => '60',
+      'on-fail'  => 'block',
+    },
+    'promote'  => {
+      'interval' => '0',
+      'timeout'  => '60',
+      'on-fail'  => 'restart',
+    },
+    'demote'  => {
+      'interval' => '0',
+      'timeout'  => '60',
+      'on-fail'  => 'stop',
+    },
+    'notify'  => {
+      'interval' => '0',
+      'timeout'  => '60',
+    },
+
+  },
 }
 
-
+# colocate postgresql resource with its vip
 cs_colocation { "pgsql_with_vip":
-      primitives => [ "master_${postgres_resource_name}:Master", "$postgres_vip_name" ],
-    }
+  primitives => [ "master_${postgres_resource_name}:Master", "$postgres_vip_name" ],
+}
 
+# order postgresql resource vip its vip during promote and demote
 cs_order { "start_vip_before_pgsql_promote":
   first   => "master_$postgres_resource_name:Promote",
   second  => "$postgres_vip_name",
