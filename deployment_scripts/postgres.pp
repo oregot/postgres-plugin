036f096a6e9587ce57414b3650a058cff9c38539
Oleg Ilin <on.ilyin@gmail.com>
2016-04-06 12:30:23 +0300

    added default config for postgres

--- deployment_scripts/postgres.pp
+++ deployment_scripts/postgres.pp
@@ -1,18 +1,18 @@
 notice('MODULAR: postgres_database/postgres.pp')
 
-$nodes_hash                     = hiera('nodes', {})
-$network_metadata = hiera_hash('network_metadata')
-$roles            = hiera('roles')
-$nodes_list       = join(keys($network_metadata[nodes])," ")
-$pgsql_vip   = $network_metadata['vips']['pgsql']['ipaddr']
-$postgres_resource_name = 'p_pgsql'
-$postgres_vip_name = 'vip__pgsql'
+$nodes_hash                = hiera('nodes', {})
+$network_metadata          = hiera_hash('network_metadata')
+$roles                     = hiera('roles')
+$nodes_list                = join(keys($network_metadata[nodes])," ")
+$pgsql_vip                 = $network_metadata['vips']['pgsql']['ipaddr']
+$postgres_resource_name    = 'p_pgsql'
+$postgres_vip_name         = 'vip__pgsql'
 $primary_controllet_int_ip = nodes_with_roles($nodes_hash, ['primary-controller'], 'internal_address')
-$postgres_hash = hiera_hash('postgresql_database')
-$postgresql_version = pick($postgres_hash['postgresql_plugin_version_database'],'9.5')
+$postgres_hash             = hiera_hash('postgresql_database')
+$postgresql_version        = pick($postgres_hash['postgresql_plugin_version_database'],'9.5')
+
 
 # Installing and configure postgresql
-
 if member($roles, 'primary-controller')
 {
 
@@ -85,6 +85,12 @@ host    replication     all     0.0.0.0/0       trust
 ',
 }  ->
 
+exec { 'Append needed config in /usr/share/postgresql/9.5/postgresql.conf.sample':
+  path    => ["/usr/bin", "/usr/sbin"],
+  name    => '/bin/echo "wal_level = hot_standby" >> /usr/share/postgresql/9.5/postgresql.conf.sample',
+  onlyif  => '/usr/bin/test ! grep ^"wal_level = hot_standby" /usr/share/postgresql/9.5/postgresql.conf.sample',
+} ->
+
 # shutting off default instance
 exec { "/usr/lib/postgresql/9.5/bin/pg_ctl -D /var/lib/postgresql/9.5/main stop":
   onlyif  => '/usr/bin/test -f "/var/lib/postgresql/9.5/main/postmaster.pid"',
@@ -94,13 +100,20 @@ exec { "/usr/lib/postgresql/9.5/bin/pg_ctl -D /var/lib/postgresql/9.5/main stop"
 
 } ->
 
+# ensure that recovery.conf is deleted
+
+file { '/var/lib/pgsql/data/recovery.conf':
+  ensure => absent,
+} ->
+
+
 # disable from autostart default instance postgresql
 exec { '/usr/sbin/update-rc.d -f postgresql remove':
   path    => ["/usr/bin", "/usr/sbin"],
 }
 
-
 }
+
 
 
 if  member($roles, 'controller') and ! member($roles, 'primary-controller')   {
@@ -136,6 +149,13 @@ exec { "pg_basebackup -h $primary_controllet_int_ip -U postgres -D /var/lib/pgsq
   onlyif  => '/usr/bin/test ! -d /var/lib/pgsql/data/base/',
 } ->
 
+# ensure that recovery.conf is deleted
+
+file { '/var/lib/pgsql/data/recovery.conf':
+  ensure => absent,
+} ->
+
+
 exec { "/usr/lib/postgresql/9.5/bin/pg_ctl -D /var/lib/postgresql/9.5/main stop":
   onlyif  => '/usr/bin/test -f "/var/lib/postgresql/9.5/main/postmaster.pid"',
   path    => ["/usr/bin", "/usr/sbin"],
@@ -153,6 +173,5 @@ exec { "/usr/sbin/pcs resource cleanup p_pgsql $fqdn":
   path    => ["/usr/bin", "/usr/sbin"],
 }
 
-
-
 }
+
